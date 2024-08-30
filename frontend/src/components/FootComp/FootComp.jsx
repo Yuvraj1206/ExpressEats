a6bc89c97dc61aec0aff3e1df028823906d7eb70
Yuvraj Saha <yuvrajsaha34@gmail.com>
2024-08-30 09:01:15 +0530

    footer watermark

--- frontend/src/components/FootComp/FootComp.jsx
+++ frontend/src/components/FootComp/FootComp.jsx
@@ -6,9 +6,14 @@ const FootComp = () => {
     <div className="foot">
       <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
         <div className="col-md-4 d-flex align-items-center">
-          <span className="mb-3 mb-md-0 text-body-secondary pl-4 ">
+          <div className="mb-3 mb-md-0 text-body-secondary pl-4 ">
             © 2024 Express Eats, Inc
-          </span>
+          </div>
+        </div>
+        <div className="col-md-4 d-flex align-items-center">
+          <div className="mb-3 mb-md-0 text-body-secondary pl-4 ">
+            created by @yuvraj_saha
+          </div>
         </div>
       </footer>
     </div>
