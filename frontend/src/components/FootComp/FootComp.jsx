03fa1d6540fb2b56262ee0ebee31e46ea8b9b192
Yuvraj Saha <yuvrajsaha34@gmail.com>
2025-05-18 11:12:39 +0530

    footer changes

--- frontend/src/components/FootComp/FootComp.jsx
+++ frontend/src/components/FootComp/FootComp.jsx
@@ -7,12 +7,12 @@ const FootComp = () => {
       <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
         <div className="col-md-4 d-flex align-items-center">
           <div className="mb-3 mb-md-0 text-body-secondary pl-4 ">
-            © 2024 Express Eats, Inc
+            © 2025 Express Eats, Inc
           </div>
         </div>
         <div className="col-md-4 d-flex align-items-center">
           <div className="mb-3 mb-md-0 text-body-secondary pl-4 ">
-            created by @yuvraj_saha
+            created by @yuvraj_saha, @samir_shaw & @gaurav_verma
           </div>
         </div>
       </footer>
